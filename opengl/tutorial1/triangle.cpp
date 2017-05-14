f204f9195e03fb224d9ac6b42d2e1ef50df9e984
artfin <artfin@mail.ru>
2017-05-15 02:03:14 +0300

    group integrals

---
+++ opengl/tutorial1/triangle.cpp
@@ -0,0 +1,61 @@
+// using standard C++ output libraries
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+// using glew.h instead of gl.h to get all the GL prototypes declared
+#include <GL/glew.h>
+#include <SDL.h>
+
+bool init_resources(void) {
+	return true;
+}
+
+void render(SDL_Window* window){
+}
+
+void free_resources() {
+}
+
+void mainLoop(SDL_Window* window) {
+	while(true) {
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev)) {
+			if (ev.type == SDL_QUIT)
+				return;
+		}
+		render(window);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	// SDL-related initialising functions
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("My first triangle", 
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			640, 480, 
+			SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+	SDL_GL_CreateContext(window);
+
+	/* Extension wrangler initialising */
+	GLenum glew_status = glewInit();
+	if (glew_status != GLEW_OK) {
+		cerr << "Error: glewInit: " << glewGetErrorString(glew_status) << endl;
+		return EXIT_FAILURE;
+	}
+
+	/* When all init functions run without errors, the program can initialise the resources */
+	if (!init_resources())
+		return EXIT_FAILURE;
+
+	/* We can display something if everything goes OK */
+	mainLoop(window);
+
+	/* If the program exits in the usual way, free resources and exit with a success */
+	free_resources();
+	return EXIT_SUCCESS;
+}
+
+
